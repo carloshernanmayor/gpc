0ea552d4bc053ac3f5ae85072d648b925440d46d
starblitzzz <santiagorodriguezsuarez2004@gmail.com>
2025-03-22 12:26:07 -0500

    restriccion de rutas por rol

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\VendedorMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,28 +27,28 @@ Route::get('imprimirvendedor','App\Http\Controllers\PdfController@imprimirVended
 Route::get('imprimirproducto','App\Http\Controllers\PdfController@imprimirProducto')->name('id_producto')->middleware('auth');
 Route::get('imprimirguion','App\Http\Controllers\PdfController@imprimirGuion')->name('id_guion')->middleware('auth');
 Route::get('imprimiratencion','App\Http\Controllers\PdfController@imprimirAtencion')->name('id_atencion')->middleware('auth');
-Route::resource('cliente', 'App\Http\Controllers\clienteController')->middleware('auth');
-Route::resource('vendedor', 'App\Http\Controllers\vendedorController')->middleware('auth');
-Route::resource('producto', 'App\Http\Controllers\productoController')->middleware('auth');
-Route::resource('guion', 'App\Http\Controllers\guionController')->middleware('auth');
-Route::resource('atencion', 'App\Http\Controllers\AtencionController')->middleware('auth');
+Route::resource('cliente', 'App\Http\Controllers\clienteController')->middleware(['auth',VendedorMiddleware::class]);
+Route::resource('vendedor', 'App\Http\Controllers\vendedorController')->middleware(['auth', AdminMiddleware::class]);
+Route::resource('producto', 'App\Http\Controllers\productoController')->middleware(['auth',VendedorMiddleware::class]);
+Route::resource('guion', 'App\Http\Controllers\guionController')->middleware(['auth',VendedorMiddleware::class]);
+Route::resource('atencion', 'App\Http\Controllers\AtencionController')->middleware(['auth',VendedorMiddleware::class]);
 
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/homeven', [App\Http\Controllers\HomeController::class, 'index'])->name('homeven');
-Route::get('/atencionadmin', [App\Http\Controllers\AtencionController::class, 'indexAdmin'])->name('atencionadmin');
-Route::get('/createatenadmin', [App\Http\Controllers\AtencionController::class, 'createAdmin'])->name('createatenadmin');
-Route::get('/editatenadmin', [App\Http\Controllers\AtencionController::class, 'editAdmin'])->name('editatenadmin');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth',AdminMiddleware::class]);;
+Route::get('/homeven', [App\Http\Controllers\HomeController::class, 'index'])->name('homeven')->middleware(['auth',VendedorMiddleware::class]);
+Route::get('/atencionadmin', [App\Http\Controllers\AtencionController::class, 'indexAdmin'])->name('atencionadmin')->middleware(['auth',AdminMiddleware::class]);;
+Route::get('/createatenadmin', [App\Http\Controllers\AtencionController::class, 'createAdmin'])->name('createatenadmin')->middleware(['auth',AdminMiddleware::class]);;
+Route::get('/editatenadmin', [App\Http\Controllers\AtencionController::class, 'editAdmin'])->name('editatenadmin')->middleware(['auth',AdminMiddleware::class]);;
 
-Route::get('/clientesadmin', [App\Http\Controllers\clienteController::class, 'indexAdmin'])->name('clientesadmin');
-Route::get('/editclientesadmin', [App\Http\Controllers\clienteController::class, 'editAdmin'])->name('editclientesadmin');
+Route::get('/clientesadmin', [App\Http\Controllers\clienteController::class, 'indexAdmin'])->name('clientesadmin')->middleware(['auth',AdminMiddleware::class]);
+Route::get('/editclientesadmin', [App\Http\Controllers\clienteController::class, 'editAdmin'])->name('editclientesadmin')->middleware(['auth',AdminMiddleware::class]);;
 
-Route::get('/productosadmin', [App\Http\Controllers\productoController::class, 'indexAdmin'])->name('productosadmin');
+Route::get('/productosadmin', [App\Http\Controllers\productoController::class, 'indexAdmin'])->name('productosadmin')->middleware(['auth',AdminMiddleware::class]);;
 
-Route::get('/guionadmin', [App\Http\Controllers\guionController::class, 'indexAdmin'])->name('guionadmin');
+Route::get('/guionadmin', [App\Http\Controllers\guionController::class, 'indexAdmin'])->name('guionadmin')->middleware(['auth',AdminMiddleware::class]);;
 
 Route::get('/profile', 'App\Http\Controllers\vendedorController@profile')->middleware('auth');
 
